afb0fb5c809dfccd06305c6f4971ce0e1f37939b
Annabella Wilkins <oav2008@gmail.com>
2021-11-26 22:41:52 -0500

    finished part B

--- src/LastnameFirstnameBusinessDriver.java
+++ src/LastnameFirstnameBusinessDriver.java
@@ -10,8 +10,22 @@ public class LastnameFirstnameBusinessDriver {
         System.out.println("I'm " + ownerName + " What is your name?");
         Scanner scr = new Scanner(System.in);
         String name = scr.nextLine();
-        String greeting = "Hey there, " + name + "!";
+        String greeting = "Here is what we serve, " + name + ". Have a look: ";
         System.out.println(greeting);
+        String[] menu = {
+                "Fried rice",
+                "Lentil Soup",
+                "Baked Falafel",
+                "Vegetarial Tacos",
+                "Soba Noodles",
+                "Paella",
+                "Minestrone soup",
+                "Red Curry",
+                "Casserole",
+                "Burrito Bowls"};
+        for (int i= 0; i<=menu.length; i++){
+            System.out.println(menu[i]);
+        }
         System.out.println(decorativeLine);
 
 
